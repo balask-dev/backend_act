72f4f3404d21967ce8de496a036faf31d9d493ea
balask-dev <balask1225@gmail.com>
2024-06-17 08:53:42 +0530

    local

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,19 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+// async fn manual_hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hey there!")
+// }
+
+#[get("/")]
+async fn hello()-> impl Responder{
+"hello"
+}
+
+#[actix_web::main]
+async fn main()-> std::io::Result<()> {
+    HttpServer::new(move || App::new().service(hello))
+    .bind(("127.0.0.1",8000))?
+    .run()
+    .await
 }
